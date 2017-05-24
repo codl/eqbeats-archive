8e774b99540c163e25c66a44f200a668282d0a0a
codl <codl@codl.fr>
2017-05-24 11:03:01 +0200

    add support for originals

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask("eqbeats-archive")
 
 s3 = boto3.client('s3')
 
+BUCKET = "eqbeats-archive"
+KEY_PREFIX = "tracks/"
+
 def url_format_to_extension(format):
     if format == "mp3":
         return "mp3"
@@ -15,7 +18,15 @@ def url_format_to_extension(format):
     else:
         return "opus"
 
-#@app.route("/track/<id>/original")
+def redir_to_s3(filename):
+    url = s3.generate_presigned_url(
+            ClientMethod='get_object',
+            Params= { 'Bucket': BUCKET, 'Key': KEY_PREFIX + filename }
+        )
+
+    return redirect(url)
+
+
 @app.route("/track/<tid>/mp3")
 @app.route("/track/<tid>/aac")
 @app.route("/track/<tid>/vorbis")
@@ -24,12 +35,14 @@ def download(tid):
     format = request.path.split("/")[-1]
     ext = url_format_to_extension(format)
 
-    bucket = "eqbeats-archive"
-    key = "tracks/%s.%s" % (tid, ext)
+    return redir_to_s3("%s.%s" % (tid, ext))
 
-    url = s3.generate_presigned_url(
-            ClientMethod='get_object',
-            Params= { 'Bucket': bucket, 'Key': key }
-        )
-
-    return redirect(url)
+@app.route("/track/<tid>/original")
+def download_original(tid):
+    resp = s3.list_objects_v2(Bucket=BUCKET,
+            Prefix=KEY_PREFIX + tid + ".orig")
+    if(resp['KeyCount'] > 0):
+        filename = resp['Contents'][0]['Key'].replace(KEY_PREFIX, "", 1)
+        return redir_to_s3(filename)
+    else:
+        return redir_to_s3(tid + ".mp3")
