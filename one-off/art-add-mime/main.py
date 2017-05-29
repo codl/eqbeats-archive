5d99b1983a682c8c21b2def5a7b59cde1aaa48c6
codl <codl@codl.fr>
2017-05-29 23:24:12 +0000

    some fixes to image mime script

--- one-off/art-add-mime/main.py
+++ one-off/art-add-mime/main.py
@@ -3,12 +3,14 @@ import boto3
 from io import BytesIO
 import mimetypes
 
-s3 = boto3.resource('s3')
-
 SOURCE_BUCKET="eqbeats-archive"
 SOURCE_PREFIX="art/"
 DEST_BUCKET="eqbeats-archive"
 DEST_PREFIX="art/"
+
+mimetypes.add_type("image/vnd.adobe.photoshop", ".psd")
+
+s3 = boto3.resource('s3')
 
 source = s3.Bucket(SOURCE_BUCKET)
 dest = s3.Bucket(DEST_BUCKET)
@@ -19,16 +21,22 @@ def get_mime(key):
     image = Image.open(f)
     mime, _ = mimetypes.guess_type("x." + image.format)
     if not mime:
-        raise "frick %" % ( image.format, )
+        raise Exception("frick %s" % ( image.format, ))
     return mime
 
 if __name__ == "__main__":
+    problematic = []
     for obj in source.objects.filter(Prefix = SOURCE_PREFIX):
-        print(obj.key)
-        mime = get_mime(obj.key)
-        print(mime)
-        dest.copy(
-                {'Key' : obj.key, 'Bucket' : SOURCE_BUCKET},
-                DEST_PREFIX + obj.key[len(SOURCE_PREFIX):],
-                {'ContentType': mime, 'ACL': 'public-read', 'MetadataDirective': 'REPLACE'}
-        )
+        try:
+            print(obj.key)
+            mime = get_mime(obj.key)
+            print(mime)
+            dest.copy(
+                    {'Key' : obj.key, 'Bucket' : SOURCE_BUCKET},
+                    DEST_PREFIX + obj.key[len(SOURCE_PREFIX):],
+                    {'ContentType': mime, 'ACL': 'public-read', 'MetadataDirective': 'REPLACE'}
+            )
+        except Exception as e:
+            print(e)
+            problematic.append(obj.key)
+    print("problems: %s", problematic)
